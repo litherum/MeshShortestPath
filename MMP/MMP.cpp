f3a0dd23b246a601ecdc87660d4d8d92eb698768
Litherum <litherum@gmail.com>
2016-12-30 12:09:29 -0800

    Connecting library to viewer

---
+++ MMP/MMP.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "MMP.h"
+
+#define CGAL_CHECK_EXPENSIVE
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/Polyhedron_incremental_builder_3.h>
+#undef CGAL_CHECK_EXPENSIVE
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+
+template <class Refs>
+class HalfedgeWithIntervalVector : public CGAL::HalfedgeDS_halfedge_base<Refs> {
+};
+
+struct MMP_Items : public CGAL::Polyhedron_items_3 {
+	template <class Refs, class Traits>
+	struct Halfedge_wrapper {
+		typedef HalfedgeWithIntervalVector<Refs> Halfedge;
+	};
+};
+
+typedef CGAL::Polyhedron_3<Kernel, MMP_Items> Polyhedron;
+
+class MMP::Impl {
+public:
+private:
+	Polyhedron polyhedron;
+};
+
+MMP::MMP(std::vector<std::array<double, 3>> pointHeap, std::vector<std::size_t> indices) : impl(std::make_unique<Impl>()) {
+
+}
+
+MMP::~MMP() {
+}
---
+++ MMP/MMP.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <array>
+#include <memory>
+#include <vector>
+
+#ifdef MMP_EXPORTS
+#define MMP_DECL __declspec(dllexport)
+#else
+#define MMP_DECL __declspec(dllimport)
+#endif
+
+class MMP_DECL MMP final {
+public:
+	MMP(std::vector<std::array<double, 3>> pointHeap, std::vector<std::size_t> indices);
+	~MMP();
+
+private:
+	class Impl;
+
+	std::unique_ptr<Impl> impl;
+};
